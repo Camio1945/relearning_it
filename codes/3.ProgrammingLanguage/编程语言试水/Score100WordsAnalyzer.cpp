69102bfbd81c3b4793c4d4aec4ffa05ca47bb6c7
HuKaiXuan <hukaixuan@up72.com>
2023-10-25 18:00:37 +0800

    C++ implementation for finding score-100 words in a dictionary

---
+++ codes/3.ProgrammingLanguage/编程语言试水/Score100WordsAnalyzer.cpp
@@ -0,0 +1,38 @@
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+int main() {
+  auto start = std::chrono::high_resolution_clock::now();
+  std::ifstream inputFile("./Dictionary.txt");
+  std::string line;
+  std::vector<std::string> score100Words;
+  while (getline(inputFile, line)) {
+    if (line.contains(' ') || line.contains('-') || line.contains('=')) {
+      continue;
+    }
+    std::istringstream is(line);
+    std::string word;
+    is >> word;
+    int score = [](const std::string &word) {
+      int score = 0;
+      for (auto &c: word) {
+        score += (c - 'a' + 1);
+      }
+      return score;
+    }(word);
+    if (score == 100) {
+      score100Words.push_back(word);
+    }
+  }
+  for (auto &word: score100Words) {
+    std::cout << word << std::endl;
+  }
+  auto end = std::chrono::high_resolution_clock::now();
+  auto time_diff = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  std::cout << "\nwords number: " << score100Words.size() << std::endl;
+  std::cout << "        time: " << time_diff.count() << "ms" << std::endl;
+  return 0;
+}
